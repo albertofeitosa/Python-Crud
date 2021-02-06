809a1be222f98a49197e49fe79d1d7fa36972750
Alberto Feitosa <albertofeitosati@gmail.com>
2021-02-06 20:24:26 -0300

    Create
    adicionado o create.

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+﻿from django.forms import ModelForm
+from app.models import Carros
+
+# Create your models here.
+class CarrosForm(ModelForm):
+    class Meta:
+        model = Carros
+        fields = ['modelo', 'marca', 'ano']
+        
--- app/views.py
+++ app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app.forms import CarrosForm
 
 # Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+
+def form(request):
+    data = {}
+    data['form'] = CarrosForm()
+    return render(request, 'form.html', data)
+
+
+def create(request):
+    form = CarrosForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
