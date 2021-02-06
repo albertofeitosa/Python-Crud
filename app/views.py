4e59227a209bdfacb395ff393484271208bba2ff
Alberto Feitosa <albertofeitosati@gmail.com>
2021-02-06 20:48:48 -0300

    Reade
    Adicionado o Read

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from app.forms import CarrosForm
+from app.models import Carros
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    data = {}
+    data['db'] = Carros.objects.all()
+    return render(request, 'index.html', data)
 
 
 def form(request):
@@ -17,3 +20,10 @@ def create(request):
     if form.is_valid():
         form.save()
         return redirect('home')
+
+
+def view(request, pk):
+    data = {}
+    data['db'] = Carros.objects.get(pk=pk)
+    return render(request, 'view.html', data)
+
